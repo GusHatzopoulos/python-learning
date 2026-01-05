fb8d66574a9aa890941a4f051336a513d8b2ed34
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-05 23:40:14 +0200

    Update kms.py

--- python-practice-projects/kms.py
+++ python-practice-projects/kms.py
@@ -3,4 +3,5 @@ kilometers = round(float(ask_miles) * 1.60934, 2)
 #kms = round(miles, 2)
 
 print(f"{ask_miles} miles is: {kilometers} kilometers.")
+
 # print(f"Entered miles value, is: {round(kms, 2)} kilometers.")
