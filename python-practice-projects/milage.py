e4c9f7e40f9686fd0402954c12ff6e86c5d8da77
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-07 20:30:14 +0200

    minor change

--- python-practice-projects/milage.py
+++ python-practice-projects/milage.py
@@ -4,3 +4,15 @@ miles = round(float(ask_kilometers) / 1.60934, 2)
 
 print(f"{ask_kilometers} kilometers, is: {miles} miles.")
 # print(f"Entered value in kilometers, is: {round(miles, 2)} miles.")
+
+
+'''
+print("How many kilometers did you cycle today?")
+kms = input() #get user input
+miles = float(kms)/1.60934 #convert from string to float and divide
+miles = round(miles, 2) #round the result
+print(f"Your {kms}km ride was {miles}mi ")  
+
+# ROUND SYNTAX:
+# round(thing to round, how many decimal points)
+'''
