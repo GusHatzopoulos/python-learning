3603c315ca24531d08deb4c9d47d26a6b1bf56ed
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-08 18:43:20 +0200

    some corrections

--- python-practice-projects/boucher.py
+++ python-practice-projects/boucher.py
@@ -1,13 +1,16 @@
 # Ask for age
-age = int(input("How old are you? "))
-
-# 18-21 wear wristband
-if age >= 18 and age <= 21:
-# if int(age) >= 18 and int(age) <= 21:
-    print("You can enter, but need a wristband.")
-# 21+ drink, normal entry
-elif age >= 21:
-    print("You can enter and can drik")
-# Too young - sorry
+age = input("How old are you? ")
+if age:
+    age = int(age)
+    if age >= 18 and age <= 21:
+    # if int(age) >= 18 and int(age) <= 21:
+            # 18-21 wear wristband
+            print("You can enter, but need a wristband.")
+    elif age >= 21:
+        # 21+ drink, normal entry
+        print("You can enter and can drik")
+    else:
+        # Too young - sorry
+        print("You are too young to enter.")
 else:
-    print("You are too young to enter.")
+    print("Please enter an age: ")
