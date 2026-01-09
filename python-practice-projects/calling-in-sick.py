aa4bc0dd57166016ab8ea1e60342fe34be26d459
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-10 01:11:21 +0200

    new practice

---
+++ python-practice-projects/calling-in-sick.py
@@ -0,0 +1,14 @@
+# NO TOUCHING ======================================
+
+from random import choice, randint
+
+# randomly assigns values to these four variables
+actually_sick = choice([True, False])
+kinda_sick = choice([True, False])
+hate_your_job = choice([True, False])
+sick_days = randint(0, 10)
+
+# NO TOUCHING ======================================
+
+
+calling_in_sick = None  # set this to True or False with Boolean Logic and Conditionals!
