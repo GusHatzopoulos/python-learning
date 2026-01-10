1ed039ea24f7d844700bb89c2caaec7d21daecb4
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-10 02:06:16 +0200

    new short project

---
+++ python-projects/rock-paper-scissors.py
@@ -0,0 +1,8 @@
+print("...Rock...")
+print("...Paper...")
+print("...Scissors...")
+
+player1 = input("(Enter Player 1's choice): ")
+player2 = input("(Enter Player 2's choice): ")
+
+print("SHOOT!")
