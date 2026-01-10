3c4dc37a3d1e1509940fa0806a88aca9f8a70eda
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-10 14:33:03 +0200

    rock-paper-scissors V1 polished a bit

--- python-projects/rock-paper-scissors.py
+++ python-projects/rock-paper-scissors.py
@@ -1,24 +1,23 @@
-# Rock, Paper, Scissors - simple version
+print("...rock...")
+print("...paper...")
+print("...scissors...")
 
-print("...Rock...")
-print("...Paper...")
-print("...Scissors...")
-
-player1 = input("(Enter Player 1's choice): ")
-player2 = input("(Enter Player 2's choice): ")
+player1 = input("(enter Player 1's choice): ")
+player2 = input("(enter Player 2's choice): ")
 
 if player1 == "rock" and player2 == "scissors":
-    print("player1 wins!")
+  print("Player1 wins!")
 elif player1 == "rock" and player2 == "paper":
-    print("player2 wins!")
+  print("Player2 wins!")
 elif player1 == "paper" and player2 == "rock":
-    print("player2 wins!")
+  print("Player1 wins!")
 elif player1 == "paper" and player2 == "scissors":
-    print("player2 wins!")
+  print("Player2 wins!")
 elif player1 == "scissors" and player2 == "rock":
-    print("player2 wins!")
+  print("Player2 wins!")
 elif player1 == "scissors" and player2 == "paper":
-  print("player1 wins!")
+  print("Player1 wins!")
 elif player1 == player2:
-    print("Its a tie!")
+  print("Its a tie!")
+
 print("SHOOT!")
