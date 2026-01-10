52f8e15356a77d7c25952154124c9c036c052f19
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-10 14:28:02 +0200

    game r-p-s V1 finished - first execution

--- python-projects/rock-paper-scissors.py
+++ python-projects/rock-paper-scissors.py
@@ -7,4 +7,18 @@ print("...Scissors...")
 player1 = input("(Enter Player 1's choice): ")
 player2 = input("(Enter Player 2's choice): ")
 
+if player1 == "rock" and player2 == "scissors":
+    print("player1 wins!")
+elif player1 == "rock" and player2 == "paper":
+    print("player2 wins!")
+elif player1 == "paper" and player2 == "rock":
+    print("player2 wins!")
+elif player1 == "paper" and player2 == "scissors":
+    print("player2 wins!")
+elif player1 == "scissors" and player2 == "rock":
+    print("player2 wins!")
+elif player1 == "scissors" and player2 == "paper":
+  print("player1 wins!")
+elif player1 == player2:
+    print("Its a tie!")
 print("SHOOT!")
