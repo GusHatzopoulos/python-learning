32a88a666e3145976724774ef81feb50dde9ed64
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-11 23:00:34 +0200

    new commit

--- python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
+++ python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
@@ -7,12 +7,7 @@ player2 = input("(enter Player 2's choice): ").lower()
 
 if player1 == "rock" and player2 == "scissors":
   print("Player1 wins!")
-elif player1 == "rock" and player2 == "paper":
-  print("Player2 wins!")
-elif player1 == "paper" and player2 == "rock":
-  print("Player1 wins!")
-elif player1 == "paper" and player2 == "scissors":
-  print("Player2 wins!")
+
 elif player1 == "scissors" and player2 == "rock":
   print("Player2 wins!")
 elif player1 == "scissors" and player2 == "paper":
