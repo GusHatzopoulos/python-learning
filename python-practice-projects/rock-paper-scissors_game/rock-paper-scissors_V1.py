e7cd0d092e719f60af25f1c3b150077b2df30ddf
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-15 19:34:37 +0200

    new test

--- python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
+++ python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
@@ -20,19 +20,4 @@ elif player1 == "scissors" and player2 == "paper":
 elif player1 == player2:
     print("Its a tie!")
 
-# if player1 == "rock" and player2 == "scissors":
-#   print("Player1 wins!")
-# elif player1 == "rock" and player2 == "paper":
-#   print("Player2 wins!")
-# elif player1 == "paper" and player2 == "rock":
-#   print("Player1 wins!")
-# elif player1 == "paper" and player2 == "scissors":
-#   print("Player2 wins!")
-# elif player1 == "scissors" and player2 == "rock":
-#   print("Player2 wins!")
-# elif player1 == "scissors" and player2 == "paper":
-#   print("Player1 wins!")
-# elif player1 == player2:
-#   print("Its a tie!")
-
 print("SHOOT!")
--- python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V2.py
+++ python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V2.py
@@ -2,28 +2,28 @@ print("...Rock...")
 print("...Paper...")
 print("...Scissors...")
 
-player1 = input("(enter Player 1's choice): ")
+player1 = input("(enter Player 1's choice): ").lower()
 print("***NO CHEATING! GAME STARTED!***\n" * 30)
-player2 = input("(enter Player 2's choice): ")
+player2 = input("(enter Player 2's choice): ").lower()
 
 if player1 == player2:
     print("Its a tie!")
 elif player1 == "rock":
     if player2 == "scissors":
-        print("Player1 wins!")
+        print("Player 1, wins!")
     elif player2 == "paper":
-        print("Player 1 wins!")
+        print("Player 1, wins!")
 elif player1 == "paper":
     if player2 == "rock":
-        print("Player1 wins!")
+        print("Player 1, wins!")
     elif player2 == "scissors":
-        print("Player2 wins!")
+        print("Player 2, wins!")
 elif player1 == "scissors":
     if player2 == "paper":
-        print("Player2 wins!")
+        print("Player 1, wins!")
     elif player2 == "rock":
-        print("Player2 wins!")
+        print("Player 2, wins!")
 else:
     print("Something went wrong.")
-
+        
 print("SHOOT!")
