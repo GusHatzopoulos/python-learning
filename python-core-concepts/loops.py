77c5010e5aef8418ba752699173f11aeb0426138
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-16 21:31:28 +0200

    new change

--- python-core-concepts/loops.py
+++ python-core-concepts/loops.py
@@ -2,6 +2,7 @@
 #     print(x)
 #     print(x*x) # get the number and then its square number
 
-for char in "coffee":
-    # print(char)
-    print(char * 10)
+# for char in "coffee":
+#     # print(char)
+#     print(char * 10)
+
