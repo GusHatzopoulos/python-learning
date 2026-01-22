e41d7efce76f86642e425a060817e3c585de10d6
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-22 19:31:22 +0200

    short fix

--- python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
+++ python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
@@ -7,7 +7,7 @@ player2 = input("Player 2 make your choice: ").lower()
 
 choices = ["rock", "paper", "scissors"]
 if player1 not in choices or player2 not in choices:
-    print('"Invalid/wrong input. Choose between "rock", "paper", "scissors".')
+    print('Invalid/wrong input. Choose between "rock", "paper", "scissors".')
 elif player1 == player2:
     print("Its a tie!")
 elif player1 == "rock" and player2 == "scissors":
