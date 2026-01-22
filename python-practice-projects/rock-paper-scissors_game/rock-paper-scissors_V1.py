81b64d44d0e81c4b2ccb131bc84d816dba97b1ee
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-22 19:32:13 +0200

    QA and fixing

--- python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
+++ python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
@@ -17,24 +17,6 @@ elif player1 == "paper" and player2 == "rock":
 elif player1 == "scissors" and player2 == "paper":
     print("Player 1 wins!")
 else:
-    print("PLayer 2 wins!")
-
-# player1 = input("Player 1, make your move: ").lower()
-# player2 = input("Player 2, make your move: ").lower()
-
-# choices = ["rock", "paper", "scissors"]
-# if player1 not in choices or player2 not in choices:
-#       print('"Invalid/wrong input. Choose between "rock", "paper", "scissors".')
-
-# elif player1 == player2:
-#     print("Its a tie!")
-# elif player1 == "rock" and player2 == "scissors":
-#         print("Player 1 wins!")
-# elif player1 == "paper" and player2 == "rock":
-#         print("Player 1 wins!")
-# elif player1 == "scissors" and player2 == "paper":
-#         print("Player 1 wins!")
-# else:
-#     print("Player 2 wins!")         
+    print("PLayer 2 wins!")      
 
 print("SHOOT!")
