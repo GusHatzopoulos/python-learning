d280e4acce47dcab00412bc9526cee708889353e
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-22 18:50:46 +0200

    some practice

--- python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
+++ python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
@@ -2,22 +2,20 @@ print("...Rock...")
 print("...Paper...")
 print("...Scissors...")
 
-player1 = input("(enter Player 1's choice): ").lower()
-player2 = input("(enter Player 2's choice): ").lower()
+player1 = input(str("Player 1, make your move: ")).lower()
+player2 = input(str("Player 2, make your move: ")).lower()
 
-if player1 == "rock" and player2 == "scissors":
-    print("Player 1, wins!")
-elif player1 == "rock" and player2 == "paper":
-    print("Player 2, wins!")
-elif player1 == "paper" and player2 == "rock":
-    print("Player 1, wins!")
-elif player1 == "paper" and player2 == "scissors":
-    print("Player 2, wins!")
-elif player1 == "scissors" and player2 == "rock":
-    print("Player 2, wins!")
-elif player1 == "scissors" and player2 == "paper":
-    print("Player 1, wins!")
-elif player1 == player2:
+if player1 == player2:
     print("Its a tie!")
+elif player1 == "rock":
+    if player2 == "scissors":
+        print("Player 1 wins!")
+    elif player2 == "paper":
+        print("Player 2 wins!")
+elif player2 == "rock":
+    if player1 == "paper":
+        print("Player 1 wins!")
+    elif player1 == "scissors":
+        print("Player 1 wins!")
 
 print("SHOOT!")
