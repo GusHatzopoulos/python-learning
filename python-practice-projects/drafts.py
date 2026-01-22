77092418da0343171c857fd4f6979f418e4f009b
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-22 20:08:13 +0200

    short refactoring and QA testing

--- python-practice-projects/drafts.py
+++ python-practice-projects/drafts.py
@@ -1,39 +1,42 @@
 from random import randint
 
-print("...Rock...")
-print("...Paper...")
-print("...Scissors...")
-
 choices = ["rock", "paper", "scissors"]
 
-player = input('Player, make your choice between rock, paper, or scissors\n" \
-    "or type "quit", "exit" or "q", to quit the gane: ').lower()
-print("***NO CHEATING! GAME STARTED!***\n" * 6)
-if player in("quit", "exit", "q"):
-    print("Quiting game. Thanks for playing!")
-    break
-if player not in choices:
-    print("Invalid choice. Choose between rock, paper, or scissors.")
-    continue
+while True:
 
-computer = choices[randint(0,2)]
+    print("----------------------------------")
+    print("...Rock...\n...Paper...\n...Scissors...")
+    # print("...Paper...")
+    # print("...Scissors...")
+    print("SHOOT!")
 
-print(f"\n************")
-print(f"Player chose {player}.")
-print(f"Computer chose {computer}.")
-print(f"\n************")
+    player = input('Player, make your choice between rock, paper, or scissors\n" \
+        "or type "quit", "exit" or "q", to quit the gane: ').lower()
+    print("***NO CHEATING! GAME STARTED!***\n" * 6)
+    if player in("quit", "exit", "q"):
+        print("Quiting game. Thanks for playing!")
+        break
+    if player not in choices:
+        print("Invalid choice. Choose between rock, paper, or scissors.")
+        continue
 
-if player not in choices or computer not in choices:
-    print('Invalid input. Choose between "rock", "paper", "scissors".')
-elif player == computer:
-    print("Its a tie!")
-elif (
-    (player == "rock" and computer == "scissors") or
-    (player == "paper" and computer == "rock") or
-    (player == "scissors" and computer == "paper")
-):
-    print("Player wins the round!")
-else:
-    print("Computer wins the round!")
+    computer = choices[randint(0,2)]
 
-print("SHOOT!")
+    print(f"\n************")
+    print(f"Player chose {player}.")
+    print(f"Computer chose {computer}.")
+    print(f"\n************")
+
+    if player not in choices or computer not in choices:
+        print('Invalid input. Choose between "rock", "paper", "scissors".')
+    elif player == computer:
+        print("Its a tie!")
+    elif (
+        (player == "rock" and computer == "scissors") or
+        (player == "paper" and computer == "rock") or
+        (player == "scissors" and computer == "paper")
+    ):
+        print("Player wins the round!")
+    else:
+        print("Computer wins the round!")
+print("----------------------------------")
