50f48f94e473f338da824da6d4f378bb204d6c64
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-22 19:00:43 +0200

    new fix

--- python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
+++ python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
@@ -2,20 +2,18 @@ print("...Rock...")
 print("...Paper...")
 print("...Scissors...")
 
-player1 = input(str("Player 1, make your move: ")).lower()
-player2 = input(str("Player 2, make your move: ")).lower()
+player1 = input("Player 1, make your move: ").lower()
+player2 = input("Player 2, make your move: ").lower()
 
 if player1 == player2:
     print("Its a tie!")
-elif player1 == "rock":
-    if player2 == "scissors":
+elif player1 == "rock" and player2 == "scissors":
         print("Player 1 wins!")
-    elif player2 == "paper":
-        print("Player 2 wins!")
-elif player2 == "rock":
-    if player1 == "paper":
+elif player1 == "paper" and player2 == "rock":
         print("Player 1 wins!")
-    elif player1 == "scissors":
-        print("Player 2 wins!")         
+elif player1 == "scissors" and player2 == "paper":
+        print("Player 1 wins!")
+else:
+    print("Player 2 wins!")         
 
 print("SHOOT!")
