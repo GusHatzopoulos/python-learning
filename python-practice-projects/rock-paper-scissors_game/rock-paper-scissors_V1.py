a29e4c354f332b4eb0add36c36d05dbeb19d1d32
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-22 19:04:44 +0200

    fixing change

--- python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
+++ python-practice-projects/rock-paper-scissors_game/rock-paper-scissors_V1.py
@@ -2,6 +2,10 @@ print("...Rock...")
 print("...Paper...")
 print("...Scissors...")
 
+choices = ["rock", "paper", "scissors"]
+if player1 not in choices or player2 not in choices:
+      print("Invalid/wrong input. Choose between "rock", "paper", "scissors."")
+
 player1 = input("Player 1, make your move: ").lower()
 player2 = input("Player 2, make your move: ").lower()
 
