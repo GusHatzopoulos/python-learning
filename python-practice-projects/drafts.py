a286865be182bccaf52b9158c2e341c3264654ee
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-23 22:21:18 +0200

    starting from scratch

--- python-practice-projects/drafts.py
+++ python-practice-projects/drafts.py
@@ -1,41 +1,41 @@
-from random import choice
+# from random import choice
 
-choices = ["rock", "paper", "scissors"]
-wins_against = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
-quit_cmds = ["quit", "exit", "q"]
+# choices = ["rock", "paper", "scissors"]
+# wins_against = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+# quit_cmds = ["quit", "exit", "q"]
 
-while True:
+# while True:
 
-    print("----------------------------------")
-    print("...Rock...\n...Paper...\n...Scissors...")
-    print("SHOOT!")
+#     print("----------------------------------")
+#     print("...Rock...\n...Paper...\n...Scissors...")
+#     print("SHOOT!")
 
-    player = input(
-        'Player, make your choice between "rock", "paper", or "scissors",\n' \
-        'or type "quit", "exit" or "q", to quit the game: '
-        ).lower()
+#     player = input(
+#         'Player, make your choice between "rock", "paper", or "scissors",\n' \
+#         'or type "quit", "exit" or "q", to quit the game: '
+#         ).lower()
     
-    print("***NO CHEATING! GAME STARTED!***\n" * 6)
+#     print("***NO CHEATING! GAME STARTED!***\n" * 6)
 
-    if player in(quit_cmds):
-        print("Quitting game. Thanks for playing!")
-        break
-    if player not in choices:
-        print("Invalid choice. Choose between rock, paper, or scissors.")
-        continue
+#     if player in(quit_cmds):
+#         print("Quitting game. Thanks for playing!")
+#         break
+#     if player not in choices:
+#         print("Invalid choice. Choose between rock, paper, or scissors.")
+#         continue
 
-    computer = choice(choices)
+#     computer = choice(choices)
 
-    print(f"\n************")
-    print(f"Player chose {player}.")
-    print(f"Computer chose {computer}.")
-    print(f"\n************")
+#     print(f"\n************")
+#     print(f"Player chose {player}.")
+#     print(f"Computer chose {computer}.")
+#     print(f"\n************")
 
-    if player == computer:
-        print("Its a tie!")
-    elif wins_against[player] == computer:
-        print("Player wins the round!")
-    else:
-        print("Computer wins the round!")
-        
-print("----------------------------------")
+#     if player == computer:
+#         print("Its a tie!")
+#     elif wins_against[player] == computer:
+#         print("Player wins the round!")
+#     else:
+#         print("Computer wins the round!")
+
+# print("----------------------------------")
