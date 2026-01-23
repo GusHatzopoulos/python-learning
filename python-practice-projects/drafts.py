e51b71441519f0a28a07aed09dfcf34356d33aa1
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-23 22:59:43 +0200

    testing player choice

--- python-practice-projects/drafts.py
+++ python-practice-projects/drafts.py
@@ -23,7 +23,7 @@ while True:
         print("Invalid choice. Choose between rock, paper, or scissors.")
         continue
     
-    print(f"DEBUG | Player choose {player}.")
+    print(f"Player choose {player}.")
 
 
 
