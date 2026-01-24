8ab1d9f0f5617fc6be464688bd19dbe703daf6ab
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-24 23:25:53 +0200

    starting the final version

---
+++ python-practice-projects/rock-paper-scissors_game/RPS_V3.1.py
@@ -0,0 +1,13 @@
+from random import choice
+
+choices = ["rock", "paper", "scissors"]
+wins_against = {"rock": "scissors", "paper": "rock", "scissors": "rock"}
+quit_cmds = ["quit", "exit", "q"]
+
+while True:
+    print("----------------------------------")
+    print("...Rock...\n...Paper...\n...Scissors...")
+    print("SHOOT!")
+
+
+    print("----------------------------------")
