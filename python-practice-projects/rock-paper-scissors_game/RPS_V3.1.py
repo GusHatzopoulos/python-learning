9176f0ed6cc71f3ca94abf52fb89e6965fbcd355
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-24 23:35:14 +0200

    testing

--- python-practice-projects/rock-paper-scissors_game/RPS_V3.1.py
+++ python-practice-projects/rock-paper-scissors_game/RPS_V3.1.py
@@ -9,5 +9,20 @@ while True:
     print("...Rock...\n...Paper...\n...Scissors...")
     print("SHOOT!")
 
+    player = input(
+        'Player, make your choice between "rock", "paper", or "scissors",\n' \
+        'or type "quit", "exit" or "q", to quit the game: '
+    ).lower()
+
+    if player in(quit_cmds):
+        print("Quiting game. Thanks for playing!")
+        break
+    if player not in(choices):
+        print("Invalid choice. Choose between rock, paper, or scissors.")
+        continue
+
+    print("***NO CHEATING!***\n" * 6)
+
+    print(f"[DEBUG] player = {player!r}.")
 
     print("----------------------------------")
