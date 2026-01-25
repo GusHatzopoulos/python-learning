ebc44f0445519ca664697cbe72860da1627b16fd
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-25 20:57:55 +0200

    testing

--- python-practice-projects/loop-and-range/repeat-excercise.py
+++ python-practice-projects/loop-and-range/repeat-excercise.py
@@ -3,3 +3,14 @@
 
 # for time in range(times):
 #     print(f"time {time+1} CLEAN UP YOUR ROOM!")
+
+nums = input("Enter a numbr for lucky and unlucky: ")
+nums = int(nums)
+
+for num in range(nums):
+    if num % 2 != 0:
+        print(f"{num} is odd!")
+    if num % 2 == 0:
+        print(f"{num} is even!")
+else:
+    print(f"{num} is unlucky!")
