8fd009830fedc08b0ba40ef1e2cf87d1bac03469
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-25 20:45:31 +0200

    new

--- python-practice-projects/loop-and-range/repeat-excercise.py
+++ python-practice-projects/loop-and-range/repeat-excercise.py
@@ -2,4 +2,4 @@ times = input("How many times do I have to tell you? ")
 times = int(times)
 
 for time in range(times):
-    print("CLEAN UP YOUR ROOM!")
+    print(f"time {time} CLEAN UP YOUR ROOM!")
