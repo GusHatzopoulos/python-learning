79367e4f839e708ed7cecd6fc80f5ee7ee93277b
GusHatzopoulos <k.hatzopoulos@protonmail.com>
2026-01-26 21:14:00 +0200

    creating a python cheatsheet

--- python-practice-projects/loop-and-range/repeat-excercise.py
+++ python-practice-projects/loop-and-range/repeat-excercise.py
@@ -4,6 +4,7 @@
 # for time in range(times):
 #     print(f"time {time+1} CLEAN UP YOUR ROOM!")
 
+'''
 nums = input("Enter a number for lucky and unlucky: ")
 nums = int(nums)
 
@@ -23,3 +24,17 @@ for num in range(nums):
     else:
         state = "even!"
     print(f"{num} is {state}")
+'''
+msg = input("Whats the secret password? ")
+while msg != "bananans":
+    print("WRONG!")
+    msg = input("Whats the secret password? ")
+print("CORRECT!")
+
+for num in range(1,11):
+    print(num)
+
+num = 1
+while num < 11:
+    print(num)
+    num += 2
